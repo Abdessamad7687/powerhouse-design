1fc1e2496d01b67a54223463b015195ff44a068d
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-08-08 16:53:53 +0100

    powerhouse-design-assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,11 @@
 import Beachy  from "./images/Beachy.png"
+import Colonial from "./images/Colonial.png"
+import Contemporary from "./images/Contemporary.png"
+import Industrial from "./images/Industrial.png"
+import Mediterranean from "./images/Mediterranean.png"
+import Minimalist from "./images/Minimalist.png"
+import Modern from "./images/Modern.png"
+import Traditional from "./images/Traditional"
 
 
-export  { Beachy }
+export  { Beachy, Colonial, Contemporary, Industrial, Mediterranean, Minimalist, Modern, Traditional }
--- src/components/PhotoList.jsx
+++ src/components/PhotoList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ImageList from '@mui/material/ImageList'
 import Container from '@mui/material/Container'
 import ImageListItem from '@mui/material/ImageListItem'
-import {Beachy} from "../assets/index"
+import {Beachy, Colonial, Contemporary, Industrial, Mediterranean, Minimalist, Modern, Traditional} from "../assets/index"
 
 const PhotoList = () => {
     const itemData = [
@@ -11,47 +11,47 @@ const PhotoList = () => {
             title: 'Bed',
         },
         {
-            img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
+            img: Colonial,
             title: 'Kitchen',
         },
         {
-            img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
+            img: Contemporary,
             title: 'Sink',
         },
         {
-            img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
+            img: Industrial,
             title: 'Books',
         },
         {
-            img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
+            img: Mediterranean,
             title: 'Chairs',
         },
         {
-            img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
+            img: Minimalist,
             title: 'Candle',
         },
         {
-            img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
+            img: Modern,
             title: 'Laptop',
         },
         {
-            img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
+            img: Traditional,
             title: 'Doors',
         },
         {
-            img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
+            img: Contemporary,
             title: 'Coffee',
         },
         {
-            img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+            img: Mediterranean,
             title: 'Storage',
         },
         {
-            img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
+            img: Industrial,
             title: 'Coffee table',
         },
         {
-            img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
+            img: Minimalist,
             title: 'Blinds',
         },
     ]
